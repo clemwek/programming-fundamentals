16ea330d10f57cbd12e7b52f54b4a04404e4cf7c
Clement Wekesa <clemwek@gmail.com>
2017-03-03 09:04:31 +0300

    Waiting to sort the throw catch

--- issue-003/solution.js
+++ issue-003/solution.js
@@ -1,3 +1,12 @@
 function product(a, b) {
     // implement your solution here
+    try {
+        if (typeof a == "string" || typeof b == "string") {
+            throw "NaN";
+        } else {
+            a * b;
+        }
+    } catch (error) {
+        return error;
+    }
 }
