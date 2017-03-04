5acb815260113172e6fedde4782a219330ca08d5
Clement Wekesa <clemwek@gmail.com>
2017-03-04 18:13:48 +0300

    Complete general sum

--- issue-008/solution.js
+++ issue-008/solution.js
@@ -1,4 +1,8 @@
 // implement your solution here
 function sum() {
-
+    var total = 0;
+    for (var i = 0; i < arguments.length; i++) {
+        total += arguments[i];
+    }
+    return total;
 }
