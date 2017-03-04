0684df4a7b2e561bef5fc087c18328bfe2183af3
Clement Wekesa <clemwek@gmail.com>
2017-03-04 18:44:58 +0300

    Compete array reduce

--- issue-009/solution.js
+++ issue-009/solution.js
@@ -1,4 +1,11 @@
 // implement your solution here
 function sumEven(a) {
-
+    return a.reduce(
+        function (total, num) {
+            if (num % 2 === 0) {
+                total += num;
+            }
+            return total;
+        }
+    );
 }
