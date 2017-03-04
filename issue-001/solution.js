fd14b88c9b8bf062e4aa95ee2425251ff2410855
Clement Wekesa <clemwek@gmail.com>
2017-03-04 10:25:28 +0300

    Complete issue-001-Data-Type-Check

--- issue-001/solution.js
+++ issue-001/solution.js
@@ -1,11 +1,9 @@
 function dataTypeCheck(a) {
     // implement your solution here
-    if  (Number(a) === a) {
-        if (a % 1 === 0) {
-            return a / 2
-        } else {
-            return a * 2
-        } 
+    if  (Number(a) === a && a % 1 === 0) {
+        return a / 2;
+    } else if (Number(a) === a && a.toString().indexOf('.') != -1) {
+        return a * 2;
     } else if ((typeof a) === "string" && Number(a) == a) {
         return Number(a);
     } else if ((typeof a) === "string" && Number(a) != a) {
