1c8bee9be7a8d4f1392bea25c0305f7fec401840
Clement Wekesa <clemwek@gmail.com>
2017-03-17 10:00:47 +0300

    Complete Old and Young

--- issue-021/solution.js
+++ issue-021/solution.js
@@ -1,4 +1,19 @@
 // implement your solution here
 function oldAndYoung(students) {
-
+    var oldest = 3000;
+    var youngest = 0;
+    var temp;
+    var outObj = {oldest: '', youngest: ''};
+    students.forEach(function(obj) {
+        temp = obj.yearOfBirth;
+        if (temp < oldest) {
+            oldest = obj.yearOfBirth;
+            outObj.oldest = obj.name;
+        }
+        if (temp > youngest) {
+            youngest = obj.yearOfBirth;
+            outObj.youngest = obj.name;
+        }
+    });
+    return outObj;   
 }
